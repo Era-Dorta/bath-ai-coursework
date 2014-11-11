ecdb06ec16f6ff54e792b7ef86acc35d12e90fae
Garoe <garoedp@gmail.com>
2014-11-11 10:35:07 +0000

    Fix loads in another data sets
    There are differences with the clas numbers, indexing, etc.
    That have to be set differently for each set

--- task1/classification/fit_bmclr_ex1.m
+++ task1/classification/fit_bmclr_ex1.m
@@ -3,9 +3,12 @@ clear all;
 
 %% Load data
 
-%load('data/MNIST_Data.mat');
-load('data/ETH-80_HoG_Data.mat');
-%load('data/UIUCSport_BoW_Data.mat')
+% {Digits, eth-80, sport_bow} = {1, 2, 3}
+data_index = 1;
+
+data_paths = {'data/MNIST_Data.mat', 'data/ETH-80_HoG_Data.mat', 'data/UIUCSport_BoW_Data.mat'};
+
+load(data_paths{data_index});
 
 num_classes = length(unique(Y));
 n_train = size(trainingIndices, 1);
@@ -13,14 +16,30 @@ n_test = size(testIndices, 1);
 
 prior = 100;
 
-% Pick the first n_train samples for training
-X_train = X(1:n_train, :);
-Y_train = Y(1:n_train);
+if data_index == 1
+    % Pick the first n_train samples for training
+    X_train = X(1:n_train, :);
+    Y_train = Y(1:n_train);
 
-% And the next n_test samples for testing
-X_test = X(n_train + 1:n_train + n_test, :);
-Y_test = Y(n_train + 1:n_train + n_test);
+    % And the next n_test samples for testing
+    X_test = X(n_train + 1:n_train + n_test, :);
+    Y_test = Y(n_train + 1:n_train + n_test);
+    
+    % The class index in this data set starts at zero, increment for
+    % matlab indexing
+    w = Y_train + 1;
+end
 
+if data_index == 2
+    % Pick the first n_train samples for training
+    X_train = X(trainingIndices, :);
+    Y_train = Y(trainingIndices);
+
+    % And the next n_test samples for testing
+    X_test = X(testIndices, :);
+    Y_test = Y(testIndices);
+    w = Y_train;
+end
 
 % Format the data for fit_mclr_bayesian
 data_n_dims = size(X_train, 2);
@@ -28,8 +47,6 @@ data_n_dims = size(X_train, 2);
 % When the data has a lot of zeros sparse matrices improve efficiency
 X_train_mclr = sparse([ones(1,size(X_train,1)); X_train']);
 X_test_mclr = sparse([ones(1,size(X_test,1)); X_test']);
-% Increase class index number for matlab easy indexing
-w = Y_train + 1;
 
 %% Profiling
 do_profile = 0;
@@ -62,8 +79,10 @@ end
 % Prediction is the one with the highest probability
 [~, predictions_class] = max(Predictions);
 
-% Decrease class index number back
-predictions_class = (predictions_class - 1)';
+if data_index == 1
+    % Decrease class index number back
+    predictions_class = (predictions_class - 1)';
+end
 
 % Get percentage of correct predictions
 array_correct_pred = Y_test - predictions_class;
