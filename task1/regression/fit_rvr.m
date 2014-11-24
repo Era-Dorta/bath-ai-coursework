9f3d5ef039ff83d0ec478a231121598fc0df8af6
Garoe <garoedp@gmail.com>
2014-11-24 14:18:47 +0000

    move commentes to beter position

--- task1/regression/fit_rvr.m
+++ task1/regression/fit_rvr.m
@@ -42,22 +42,22 @@ function [mu_test, var_test, relevant_points] = ...
     K_w = K*w;
     
     % The main loop.
-    iterations_count = 0;
+    %iterations_count = 0;
     precision = 0.001;
     while true
         % Compute the variance. Use the range [0,variance of world values].
         % Constrain var to be positive, by expressing it as
         % var=sqrt(var)^2, that is, the standard deviation squared.
-        mu_world = sum(w) / I;
         %8.56
+        mu_world = sum(w) / I;
         var_world = sum((w - mu_world) .^ 2) / I;
         var = fminbnd (@(var) fit_rvr_cost (var, K, w, H), 0, var_world);
         
+        %Equation 8.57
         % Update sig and mu.
         sig = inv (K_K/var + diag(H));
         mu = sig*K_w/var;
-        %Equation 8.57
-        
+                
         % Update H, equation 8.55
         H = H .* diag(sig);
         H = nu + 1 - H;
