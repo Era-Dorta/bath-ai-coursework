eb570fe870812d06cc8f4b91bdeb36a315218c91
Garoe <garoedp@gmail.com>
2014-11-24 14:08:48 +0000

    Given data in the proper format to the git_rvr

--- task1/regression/task1_2.m
+++ task1/regression/task1_2.m
@@ -1,5 +1,8 @@
+%% Clear all
 clear all;
 close all;
+
+%% Load data
 
 load('data/Fonts_n_to_m.mat');
 
@@ -11,12 +14,26 @@ X_test = [ones(1, n_test); X(testIndices, :)'];
 
 colors = ['b','g','r','c','m','y','k', 'b','g','r','c','m','y','k'];
 
-figure;
-for i=1:size(X, 1)
-    plotCharacter(X(i, :), strcat(colors(i),'-'));
-end
+w = Y(trainingIndices,:);
 
-figure;
-for i=1:size(X, 1)
-    plotCharacter(Y(i, :), strcat(colors(i),'-'));
-end
+nu = 0.0005;
+
+kernel = @(x_i, x_j) kernel_gauss (x_i, x_j, 2);
+
+%% Fit model
+
+[mu_test, var_test, relevant] = fit_rvr (X_train, w, nu, X_test, kernel);
+
+%% Check predictions
+
+Y_test = Y(testIndices, :);
+
+% figure;
+% for i=1:size(X, 1)
+%     plotCharacter(X(i, :), strcat(colors(i),'-'));
+% end
+% 
+% figure;
+% for i=1:size(X, 1)
+%     plotCharacter(Y(i, :), strcat(colors(i),'-'));
+% end
