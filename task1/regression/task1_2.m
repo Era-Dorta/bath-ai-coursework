e6a3c6cca2b9ae8532cb8bab090ae21900667214
Garoe <garoedp@gmail.com>
2014-11-24 11:35:20 +0000

    Added sample code for rvr from the book
    As spected in only works on one dimension

--- task1/regression/task1_2.m
+++ task1/regression/task1_2.m
@@ -3,12 +3,20 @@ close all;
 
 load('data/Fonts_n_to_m.mat');
 
+n_train = size(trainingIndices, 1);
+n_test = size(testIndices, 1);
+
+X_train = [ones(1, n_train); X(trainingIndices, :)'];
+X_test = [ones(1, n_test); X(testIndices, :)'];
+
+colors = ['b','g','r','c','m','y','k', 'b','g','r','c','m','y','k'];
+
 figure;
 for i=1:size(X, 1)
-    plotCharacter(X(i, :), 'b-');
+    plotCharacter(X(i, :), strcat(colors(i),'-'));
 end
 
 figure;
 for i=1:size(X, 1)
-    plotCharacter(Y(i, :), 'b-');
+    plotCharacter(Y(i, :), strcat(colors(i),'-'));
 end
