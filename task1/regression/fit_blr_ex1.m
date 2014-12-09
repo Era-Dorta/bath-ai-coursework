006adbbcc793e8a6bf90a1fd65eff22d58bd1edf
Garoe <garoedp@gmail.com>
2014-12-09 15:16:17 +0000

    Fixed input values for bayesian linear regression

--- task1/regression/fit_blr_ex1.m
+++ task1/regression/fit_blr_ex1.m
@@ -1,9 +1,12 @@
+clear all;
+close all;
+
 %% Load data
 data_path = 'data/Fonts_n_to_m.mat';
 
 load(data_path);
 
-var_prior = 6;
+var_prior = 0.1;
 
 n_train = size(trainingIndices, 1);
 n_test = size(testIndices, 1);
@@ -16,27 +19,25 @@ w = Y(trainingIndices,:);
 D = size(X,2);
 
 mu_test = zeros(n_test, D);
-% Fit Bayesian linear regression model.
+var_test = zeros(n_test, D);
+
+% Fit Bayesian linear regression model for each dimension in w.
 for d=1:D
-    [mu_test(:,d), var_test, var, A_inv] = fit_blr (X_train(d,:), w(:,d), var_prior, X_test(d,:));
+    [mu_test(:,d), var_test(:,d), ~, ~] = fit_blr (X_train, w(:,d), var_prior, X_test);
 end
 
-
-plotNormalized = 0;
 plotDev = 0;
+
 Y_test = Y(testIndices, :);
 for i=1:n_test
     figure;
     plotCharacter(Y(testIndices(i), :), 'b-');
-    if plotNormalized
-        mu_test(i,:) = norm(Y_test(i,:))* mu_test(i,:) / norm(mu_test(i,:));
-        var_test(i,:) = norm(Y_test(i,:))* var_test(i,:) / norm(mu_test(i,:));
-    end
     plotCharacter(mu_test(i, :), 'r-');
+    
     if plotDev
-        standardDeviation = var_test(i,:).^0.5;
-        varMax = max(mu_test(i, :) + 2*standardDeviation,mu_test(i, :) - 2*standardDeviation);
-        varMin = min(mu_test(i, :) + 2*standardDeviation,mu_test(i, :) - 2*standardDeviation);
+        standardDeviation = 0.1 * var_test(i,:).^0.5;
+        varMax = mu_test(i, :) + standardDeviation;
+        varMin = mu_test(i, :) - standardDeviation;
         plotCharacter(varMax, 'k.');
         plotCharacter(varMin, 'k.');
     end
